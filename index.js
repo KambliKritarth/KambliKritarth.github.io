7d1864a5974952e51fe98c5c45d171497029724f
Kritarth Kambli <kritarthkambli@gmail.com>
2023-03-10 09:37:23 +0530

    self portfolio

--- index.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById("menu");
-
-Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.addEventListener("mouseover", function () {
-      menu.dataset.activeIndex = index;
-    })
-  });
